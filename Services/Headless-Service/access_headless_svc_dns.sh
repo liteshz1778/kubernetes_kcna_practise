9beecd22827f21279360faffb65ba934c33705f2
= <=>
2026-06-05 09:44:54 +0000

    Added changes to code of  Services/Headless-Service/ - 05/05/2026

--- Services/Headless-Service/access_headless_svc_dns.sh
+++ Services/Headless-Service/access_headless_svc_dns.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 kubectl run -it \
+  --restart=Never \
   --image=busybox busybox \
   -- nslookup mysql-sts-0.mysql-headless-svc.default.svc.cluster.local
 
-kubectl delete po busybox
+#kubectl delete po busybox
