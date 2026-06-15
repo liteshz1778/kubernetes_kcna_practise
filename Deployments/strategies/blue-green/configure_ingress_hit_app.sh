565501c802f71ef26bf2443dd7b8e13698e26d43
= <=>
2026-06-15 12:12:23 +0000

    Added code for Deployments/strategies/blue-green/ - 15/06/2026

---
+++ Deployments/strategies/blue-green/configure_ingress_hit_app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+kubectl apply -f ./ingress.yml
+echo;
+
+INGRESS_NODEPORT=$(kubectl get svc -n ingress-nginx ingress-nginx-controller -o json | jq '.spec.ports[0].nodePort')
+echo "Configured Ingress Port is: ${INGRESS_NODEPORT}"
+echo;
+
+echo "Hitting Httpd WebServer Application!!"
+echo;
+for i in $(seq 1 20); do curl -s http://127.0.0.1:${INGRESS_NODEPORT}/ -H "Host: my-demo-svc.com"  | grep "Httpd WebServer Application"; done
